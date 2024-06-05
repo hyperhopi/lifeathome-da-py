f98e8381720053dbd3963b5d4e04c439cdbe7ead
hyperhopi <hopi@LAPTOP-J2KFTC6S>
2024-06-05 13:53:13 +0200

    bring raw data and make neweditable one

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,16 @@
+import os
 import pandas as pd
 
-file_path = file_path = 'C:\\Users\\Hopi\\OneDrive\\local and cloud\\DA\\module\\data\\data.xlsx'
+directory_path = 'C:/Users/Hopi/Awork/da-assignment'
+
+file_name = 'data.xlsx'
+file_path = os.path.join(directory_path, file_name)
 
 df = pd.read_excel(file_path, sheet_name='Sheet1')
 
+output_file_name = 'cleaned_data.xlsx'
+output_file_path = os.path.join(directory_path, output_file_name)
+
+df.to_excel(output_file_path, index=False)
 
 print(df.head())
