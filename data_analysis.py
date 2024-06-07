6cf7cd0f883afc2cbdd1eacf33dafe36c697a8c2
hyperhopi <hyewon.park@hyperisland.se>
2024-06-07 23:35:38 +0200

    cleaning data + remain only few focus factors

--- data_analysis.py
+++ data_analysis.py
@@ -1,15 +1,37 @@
 import os
 import pandas as pd
+
+# Set directory path and file name
 directory_path = 'C:/Users/Hopi/Awork/da-assignment'
 file_name = 'data.xlsx'
 file_path = os.path.join(directory_path, file_name)
 
 df = pd.read_excel(file_path, sheet_name='Sheet1')
 
-df['2_INGKA/Non-INGKA'] = df['2_INGKA/Non-INGKA'].replace('INGKA', 'INGKAS')
+# Print data 1
+print(df.head())
 
+# Data cleaning
+columns_of_interest = [
+    'Country', '3_Gender', '4_Age', '5_Generation', '234_Happy', 
+    '235_Relaxed_and_content', '236_Connected_to_others', 
+    '230_Secure', '238_Productive', '239_Hopeful_for_the_future'
+]
+filtered_data = df[columns_of_interest]
+
+filtered_data.columns = [
+    'Country', 'Gender', 'Age', 'Generation', 'Happy', 
+    'Relaxed_and_Content', 'Connected_to_Others', 
+    'Secure', 'Productive', 'Hopeful_for_the_Future'
+]
+
+# Print data 2
+print(filtered_data.head())
+
+# 변경된 데이터 저장
 output_file_name = 'cleaned_data.xlsx'
 output_file_path = os.path.join(directory_path, output_file_name)
 
-df.to_excel(output_file_path, index=False)
-print(df.head())
+filtered_data.to_excel(output_file_path, index=False)
+
+print(f"Filtered data saved to: {output_file_path}")
