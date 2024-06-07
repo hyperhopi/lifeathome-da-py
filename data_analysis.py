9592b740a5be1744ac877837ee17d8505520def0
hyperhopi <hyewon.park@hyperisland.se>
2024-06-07 22:50:17 +0200

    reconnect fix

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,6 @@
 import os
 import pandas as pd
-
 directory_path = 'C:/Users/Hopi/Awork/da-assignment'
-
 file_name = 'data.xlsx'
 file_path = os.path.join(directory_path, file_name)
 
@@ -14,5 +12,4 @@ output_file_name = 'cleaned_data.xlsx'
 output_file_path = os.path.join(directory_path, output_file_name)
 
 df.to_excel(output_file_path, index=False)
-
 print(df.head())
